3aae6c881c4e1c9571cf306cd269356d994b99de
11249A145_oops_labrecord <11249a145@kanchiuniv.ac.in>
2026-04-23 23:51:32 +0530

    Create Hanoi(Recursion).c

---
+++ Hanoi(Recursion).c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void tower(int n, char from, char to, char aux)
+{
+    if(n == 1)
+    {
+        printf("Move disk 1 from %c to %c\n", from, to);
+        return;
+    }
+
+    tower(n-1, from, aux, to);
+
+    printf("Move disk %d from %c to %c\n", n, from, to);
+
+    tower(n-1, aux, to, from);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+
+    if(n <= 0)
+    {
+        printf("Number of disks must be greater than 0\n");
+        return 0;
+    }
+
+    tower(n, 'A', 'C', 'B');
+
+    return 0;
+}
